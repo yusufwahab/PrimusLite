f1bba9ad7b66008fea0411ff445fc3968fd3dfed
yusufwahab <yusufabdulwhab@gmail.com>
2025-07-11 16:03:52 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import Dashboard from "./Dashboard";
-import CameraCard from "./CameraCard";
 import Header from "./Header";
-import LogoLoader from "./LogoLoader";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
